c4689ad4341cc72a7f0f52faa3c81584bbf54e99
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2017-05-09 12:31:31 +0200

    Cython modules for RTD

--- tslearn/docs/conf.py
+++ tslearn/docs/conf.py
@@ -18,8 +18,11 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('../../'))
-#sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../..'))
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../..'))
+else:  #locally
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../..'))
 
 # -- General configuration ------------------------------------------------
 
