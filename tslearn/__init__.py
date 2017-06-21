4fe02ac12bc10be50d867f6554927555550392e6
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2017-06-21 18:14:29 +0200

    Doc fix

--- tslearn/__init__.py
+++ tslearn/__init__.py
@@ -1,2 +1,9 @@
+import os
+
 __author__ = 'Romain Tavenard romain.tavenard[at]univ-rennes2.fr'
 __version__ = "0.0.23"
+
+
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    import pyximport; pyximport.install()
