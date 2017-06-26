d87f6a24eea819ac34eadefbec98c4791c78a5a5
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2017-06-26 16:14:30 +0200

    SoftDTW barycenter interpolate

---
+++ tslearn/docs/examples/plot_barycenter_interpolate.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Soft-DTW weighted barycenters
+=============================
+
+This example presents the weighted Soft-DTW time series barycenter method.
+"""
+
+# Code source: Romain Tavenard
+# License: BSD 3 clause
+
+import numpy
+import matplotlib.pyplot as plt
+import matplotlib.colors
+
+from tslearn.preprocessing import TimeSeriesScalerMinMax
+from tslearn.barycenters import SoftDTWBarycenter
+from tslearn.datasets import CachedDatasets
+
+
+def row_col(pos, n_cols=5):
+    idx_row = (pos - 1) // n_cols
+    idx_col = pos - n_cols * idx_row - 1
+    return idx_row, idx_col
+
+
+def get_color(weights):
+    baselines = numpy.zeros((4, 3))
+    weights = numpy.array(weights).reshape(1, 4)
+    for i, c in enumerate(["r", "g", "b", "y"]):
+        baselines[i] = matplotlib.colors.ColorConverter().to_rgb(c)
+    return numpy.dot(weights, baselines).ravel()
+
+n_ts, sz, d = 20, 128, 1
+
+numpy.random.seed(0)
+X_train, y_train, X_test, y_test = CachedDatasets().load_dataset("Trace")
+X_out = numpy.empty((4, X_train.shape[1], X_train.shape[2]))
+
+plt.figure()
+for i in range(4):
+    X_out[i] = X_train[y_train == (i + 1)][0]
+X_out = TimeSeriesScalerMinMax().fit_transform(X_out)
+
+for i, pos in enumerate([1, 5, 21, 25]):
+    plt.subplot(5, 5, pos)
+    w = [0.] * 4
+    w[i] = 1.
+    plt.plot(X_out[i].ravel(),
+             color=matplotlib.colors.rgb2hex(get_color(w)),
+             linewidth=2)
+    plt.xticks([])
+    plt.yticks([])
+
+for pos in range(2, 26):
+    if pos in [1, 5, 21, 25]:
+        continue
+    plt.subplot(5, 5, pos)
+    idxr, idxc = row_col(pos, 5)
+    w = [0.] * 4
+    w[0] = (4 - idxr) * (4 - idxc) / 16
+    w[1] = (4 - idxr) * idxc / 16
+    w[2] = idxr * (4 - idxc) / 16
+    w[3] = idxr * idxc / 16
+    plt.plot(SoftDTWBarycenter(weights=w).fit(X_out).ravel(),
+             color=matplotlib.colors.rgb2hex(get_color(w)),
+             linewidth=2)
+    plt.xticks([])
+    plt.yticks([])
+
+
+plt.tight_layout()
+plt.show()
