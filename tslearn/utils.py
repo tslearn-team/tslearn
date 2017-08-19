e1813c0e0bfb30990e9fd825661dee109cbd9b64
rtavenar <romain.tavenard@univ-rennes2.fr>
2017-08-19 10:31:15 +0200

    Added test for file operations

--- tslearn/utils.py
+++ tslearn/utils.py
@@ -231,6 +231,14 @@ def load_timeseries_txt(fname):
     numpy.ndarray or list of arrays
         The dataset of time series.
 
+    Example
+    -------
+    >>> dataset = to_time_series_dataset([[1, 2, 3, 4], [1, 2, 3]], equal_size=False)
+    >>> save_timeseries_txt("tmp-tslearn-test.txt", dataset)
+    >>> reloaded_dataset = load_timeseries_txt("tmp-tslearn-test.txt")
+    >>> [numpy.alltrue((ts0 - ts1) < 1e-6) for ts0, ts1 in zip(dataset, reloaded_dataset)]
+    [True, True]
+
     See Also
     --------
     save_timeseries_txt : Save time series to disk
