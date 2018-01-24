8d6bba1ae64f24c73e71e8d13e9c91fad9d8c7b9
cr458 <christopher.x.ren@gmail.com>
2018-01-24 08:58:44 +0000

    used function in test, d'oh

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,7 +9,7 @@ from tslearn.utils import _arraylike_copy
 	 (np.array([6]), np.array([6]))
 	])
 def test_arraylike_copy(test_array, expected_output):
-	assert_array_equal(test_array, expected_output)
+	assert_array_equal(_arraylike_copy(test_array), expected_output)
 	#check deep copy for numpy array input
 	assert not np.may_share_memory(test_array, expected_output)
 
