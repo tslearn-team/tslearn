678da13d1354df19b566a9b234e6eafb8db6b090
cr458 <christopher.x.ren@gmail.com>
2018-01-28 10:02:12 +0000

    test for bit length

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,7 @@ import numpy as np
 from numpy.testing import assert_array_equal
 import pytest
 from tslearn.utils import _arraylike_copy
+from tslearn.utils import bit_length
 
 @pytest.mark.parametrize(
 	"test_array, expected_output",
@@ -16,6 +17,12 @@ def test_arraylike_copy(test_array, expected_output):
 	#check deep copy for numpy array input
 	assert not np.shares_memory(test_array, _arraylike_copy(test_array))
 
+def test_bit_length():
+	test_input = 10
+	expected_bit_length = 4
+	test_bit_length = bit_length(test_input)
+	assert test_bit_length == expected_bit_length
 
+def  
 
 
