f2e9fa1a0117c302db8b5c68dc9f7299bc6b7aea
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2018-04-19 17:50:53 +0200

    new version of tslearn

--- tslearn/__init__.py
+++ tslearn/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 __author__ = 'Romain Tavenard romain.tavenard[at]univ-rennes2.fr'
-__version__ = "0.1.10.8"
+__version__ = "0.1.11"
 
 
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
