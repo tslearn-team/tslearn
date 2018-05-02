f7b983cdfa514f4ae0b0fbb86b6cfcb584d7af4f
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2018-05-02 09:43:13 +0200

    Bugfix: illegal default parameter (cf. Issue #32 )

--- tslearn/__init__.py
+++ tslearn/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 __author__ = 'Romain Tavenard romain.tavenard[at]univ-rennes2.fr'
-__version__ = "0.1.16.1"
+__version__ = "0.1.16.2"
 
 
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
