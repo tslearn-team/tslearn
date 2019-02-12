89f78d3d810ac282018bc4aaa497a8e54453ed83
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2019-02-12 12:44:53 +0100

    Attempt to make numpy dependency install smoothly

--- setup.py
+++ setup.py
@@ -1,22 +1,34 @@
 from setuptools import setup, Extension
-import numpy
 import tslearn
 
 from Cython.Distutils import build_ext as _build_ext
 
+
+class CustomBuildExtCommand(_build_ext):
+    """build_ext command for use when numpy headers are needed."""
+    def run(self):
+
+        # Import numpy here, only when headers are needed
+        import numpy
+
+        # Add numpy headers to include_dirs
+        self.include_dirs.append(numpy.get_include())
+
+        # Call original build_ext command
+        _build_ext.run(self)
+
 list_pyx = ['cydtw', 'cygak', 'cysax', 'cycc', 'soft_dtw_fast']
-ext = [Extension('tslearn.%s' % s, ['tslearn/%s.pyx' % s], include_dirs=[numpy.get_include()]) for s in list_pyx]
+ext = [Extension('tslearn.%s' % s, ['tslearn/%s.pyx' % s]) for s in list_pyx]
 
 setup(
     name="tslearn",
     description="A machine learning toolkit dedicated to time-series data",
-    include_dirs=[numpy.get_include()],
     packages=['tslearn'],
     package_data={"tslearn": [".cached_datasets/Trace.npz"]},
     data_files=[("", ["LICENSE"])],
     install_requires=['numpy', 'scipy', 'scikit-learn', 'Cython'],
     ext_modules=ext,
-    cmdclass={'build_ext': _build_ext},
+    cmdclass={'build_ext': CustomBuildExtCommand},
     version=tslearn.__version__,
     url="http://tslearn.readthedocs.io/",
     author="Romain Tavenard",
