a86bf2038cb10a546ab2d9d7a346b7d48d44bad5
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2019-04-02 09:36:35 +0200

    Fix doctest for `sigma_l` value

--- tslearn/piecewise.py
+++ tslearn/piecewise.py
@@ -498,7 +498,7 @@ class OneD_SymbolicAggregateApproximation(SymbolicAggregateApproximation):
     Traceback (most recent call last):
     ValueError: Model not fitted yet: cannot be used for distance computation.
     >>> unfitted_1dsax.fit(data).sigma_l  # doctest: +ELLIPSIS
-    0.07071...
+    0.12247...
 
 
     References
