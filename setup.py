03c321500d7d4e216d3993551f905ab1025a919d
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2019-06-27 21:57:54 +0200

    Removed import from tslearn in setup (@rflamary trick)

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from setuptools import setup, Extension
 import numpy
-import tslearn
+import re
 
 from Cython.Distutils import build_ext as _build_ext
+
+# dirty but working (from POT)
+__version__ = re.search(
+    r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',  # It excludes inline comment too
+    open('tslearn/__init__.py').read()).group(1)
+# The beautiful part is, I don't even need to check exceptions here.
+# If something messes up, let the build process fail noisy, BEFORE my release!
+
+# thanks Pipy for handling markdown now
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(ROOT, 'README.md'), encoding="utf-8") as f:
+    README = f.read()
 
 list_pyx = ['cygak', 'cysax', 'cycc', 'soft_dtw_fast']
 ext = [Extension('tslearn.%s' % s, ['tslearn/%s.pyx' % s],
@@ -12,6 +25,8 @@ ext = [Extension('tslearn.%s' % s, ['tslearn/%s.pyx' % s],
 setup(
     name="tslearn",
     description="A machine learning toolkit dedicated to time-series data",
+    long_description=README,
+    long_description_content_type='text/markdown',
     include_dirs=[numpy.get_include()],
     packages=['tslearn'],
     package_data={"tslearn": [".cached_datasets/Trace.npz"]},
@@ -19,7 +34,7 @@ setup(
     install_requires=['numpy', 'scipy', 'scikit-learn', 'Cython', 'numba'],
     ext_modules=ext,
     cmdclass={'build_ext': _build_ext},
-    version=tslearn.__version__,
+    version=__version__,
     url="http://tslearn.readthedocs.io/",
     author="Romain Tavenard",
     author_email="romain.tavenard@univ-rennes2.fr"
