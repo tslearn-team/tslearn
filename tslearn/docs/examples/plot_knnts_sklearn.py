66818160cc5c16bee7ce4c5596e3b48047b74e25
Gilles Vandewiele <givdwiel.vandewiele@ugent.be>
2019-07-02 16:52:29 +0200

    nicer output of test script & pep8 issue

--- tslearn/docs/examples/plot_knnts_sklearn.py
+++ tslearn/docs/examples/plot_knnts_sklearn.py
@@ -9,12 +9,21 @@ import numpy as np
 
 # In this example, we show how tslearn estimators can be used in combination
 # with sklearn utilities such as Pipeline
+N_SPLITS = 3
 pipeline = GridSearchCV(Pipeline([
     ('normalize', TimeSeriesScalerMinMax()),
     ('knn', KNeighborsTimeSeriesClassifier())
-]), {'knn__n_neighbors': [3, 5, 7]}, cv=2)
+]), {'knn__n_neighbors': [3, 5, 7]}, cv=N_SPLITS, iid=True)
 
-X, y = random_walk_blobs(n_ts_per_blob=50, sz=15, d=1)
+X, y = random_walk_blobs(n_ts_per_blob=25, sz=15, d=2, noise_level=2., 
+                         random_state=42)
 X = X * 10
 pipeline.fit(X, y)
-print(pipeline.cv_results_)
+results = pipeline.cv_results_
+
+print('Fitted KNeighborsTimeSeriesClassifier on random walk blobs...')
+for i in range(len(results['params'])):
+	s = '{}\t'.format(results['params'][i])
+	for k in range(N_SPLITS):
+		s += '{}\t'.format(results['split{}_test_score'.format(k)][i])
+	print(s.strip())
