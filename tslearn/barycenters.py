e64b9f3325956b3fc4deece4ec6d4a506e9d3b5f
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2019-08-26 23:16:30 +0200

    Update barycenters.py

--- tslearn/barycenters.py
+++ tslearn/barycenters.py
@@ -57,10 +57,10 @@ def euclidean_barycenter(X, weights=None):
     >>> bar.shape
     (4, 1)
     >>> bar
-    array([[ 1. ],
-           [ 2. ],
-           [ 3.5],
-           [ 4.5]])
+    array([[1. ],
+           [2. ],
+           [3.5],
+           [4.5]])
     """
     X_ = to_time_series_dataset(X)
     weights = _set_weights(weights, X_.shape[0])
