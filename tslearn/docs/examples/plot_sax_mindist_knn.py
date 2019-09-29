8447e33f5a493a936d5cc142ec45327ab414e778
Gilles Vandewiele <givdwiel.vandewiele@ugent.be>
2019-09-29 12:32:30 +0200

    cleaned up example code

--- tslearn/docs/examples/plot_sax_mindist_knn.py
+++ tslearn/docs/examples/plot_sax_mindist_knn.py
@@ -13,6 +13,7 @@ paper is used as a distance metric.
 
 import numpy
 import matplotlib.pyplot as plt
+import time
 
 from tslearn.datasets import UCR_UEA_datasets
 from tslearn.preprocessing import TimeSeriesScalerMeanVariance
@@ -29,61 +30,40 @@ from sklearn.neighbors import KNeighborsClassifier
 
 from scipy.stats import norm
 
+import warnings
+warnings.filterwarnings('ignore')
+
 numpy.random.seed(0)
 # Generate a random walk time series
 data_loader = UCR_UEA_datasets()
 datasets = [
-    ##('SyntheticControl', 16),
+    ('SyntheticControl', 16),
     ('GunPoint', 64),
     #('CBF', 32),
     #('FaceAll', 64),
-    ##('OSULeaf', 128),
+    ('OSULeaf', 128),
     #('SwedishLeaf', 32),
     #('FiftyWords', 128),
     ('Trace', 128),
     #('TwoPatterns', 32),
-    ##('FaceFour', 128),
-    ##('Lightning2', 256),
-    ##('Lightning7', 128),
-    ##('ECG200', 32),
+    ('FaceFour', 128),
+    ('Lightning2', 256),
+    ('Lightning7', 128),
+    ('ECG200', 32),
     #('Adiac', 64),
     #('Yoga', 128),
-    ##('Fish', 128),
-    ##('Plane', 64),
-    ##('Car', 256),
-    ##('Beef', 128),
-    ##('Coffee', 128),
-    ##('OliveOil', 256)
+    ('Fish', 128),
+    ('Plane', 64),
+    ('Car', 256),
+    ('Beef', 128),
+    ('Coffee', 128),
+    ('OliveOil', 256)
 ]
 
-def generate_lookup_table(a):
-    return [norm.ppf(i * (1./a)) for i in range(1, a)]
-
-def calc_distances(X, y=None):
-    X = X.reshape((X.shape[0], X.shape[1]))
-    cardinality = numpy.max(X) + 1
-    n = X_train.shape[1]
-    w = X.shape[1]
-    table = generate_lookup_table(cardinality)
-
-    def point_dist(i, j):
-        i, j = int(i), int(j)
-        if abs(i - j) <= 1:
-            return 0
-        else:
-            return table[max(i, j) - 1] - table[min(i, j)]
-
-    def sax_mindist(x, y):
-        point_dists = [point_dist(x[i], y[i]) ** 2 for i in range(w)]
-        return numpy.sqrt(n / w) * numpy.sqrt(numpy.sum(point_dists))
-
-
-    return pairwise_distances(X, metric=sax_mindist)
-
-def print_table(accuracies):
+def print_table(accuracies, times):
     header_str = '|'
     header_str += '{:^20}|'.format('dataset')
-    columns = ['sax error', 'eucl error']
+    columns = ['sax error', 'sax time', 'eucl error', 'eucl time']
     for col in columns:
         header_str += '{:^12}|'.format(col)
     print(header_str)
@@ -91,101 +71,41 @@ def print_table(accuracies):
 
     for dataset in accuracies:
         acc_sax, acc_euclidean = accuracies[dataset]
+        time_sax, time_euclidean = times[dataset]
         sax_error = numpy.around(1 - acc_sax, 5)
         eucl_error = numpy.around(1 - acc_euclidean, 5)
+        time_sax = numpy.around(time_sax, 5)
+        time_euclidean = numpy.around(time_euclidean, 5)
         s = '|'
         s += '{:>20}|'.format(dataset)
         s += '{:>12}|'.format(sax_error)
         s += '{:>12}|'.format(eucl_error)
+        s += '{:>12}|'.format(eucl_error)
+        s += '{:>12}|'.format(time_euclidean)
         print(s.strip())
 
     print('-'*(len(columns) * 13 + 22))
 
+accuracies = {}
+times = {}
 for dataset, w in datasets:
     X_train, y_train, X_test, y_test = data_loader.load_dataset(dataset)
     metric_params = {'n_segments': w, 'alphabet_size_avg': 10}
     knn = KNeighborsTimeSeriesClassifier(n_neighbors=1, metric='sax',
                                          metric_params=metric_params)
+    start = time.time()
     knn.fit(X_train, y_train)
-    knts_dist, knts_ind = knn.kneighbors(X_test)
-    print(knn.predict(X_test))
-
-    print(accuracy_score(y_test, knn.predict(X_test)))
-
-    pipe = Pipeline([
-        (
-            'transform', 
-            SymbolicAggregateApproximation(n_segments=w, alphabet_size_avg=10)
-        ),
-        (
-            'distance', 
-            FunctionTransformer(calc_distances, validate=False, pass_y=False)
-        )
-    ])
-
-    all_X = numpy.vstack((X_train, X_test))
-    distances = pipe.transform(all_X)
-
-    # We only need the distances to the timeseries from the training set.
-    # Both for the test and the training set.
-    X_train_dist = distances[:len(X_train), :len(X_train)]
-    X_test_dist = distances[len(X_train):, :len(X_train)]
-
-    knn = KNeighborsClassifier(n_neighbors=1, metric='precomputed')
-    knn.fit(X_train_dist, y_train)
-    print(knn.predict(X_test_dist))
-    knn_dist, knn_ind = knn.kneighbors(X_test_dist)
-    acc_sax = accuracy_score(y_test, knn.predict(X_test_dist))
-    print(acc_sax)
-
-    print(list(zip([x[0] for x in knts_dist], [x[0] for x in knn_dist])))
-    print(list(zip([x[0] for x in knts_ind], [x[0] for x in knn_ind])))
-    input()
+    acc_sax = accuracy_score(y_test, knn.predict(X_test))
+    time_sax = time.time() - start
 
     knn = KNeighborsTimeSeriesClassifier(n_neighbors=1, metric='euclidean')
+    start = time.time()
     knn.fit(X_train, y_train)
     predictions = knn.predict(X_test)
     acc_euclidean = accuracy_score(y_test, predictions)
-    print(acc_euclidean)
+    time_euclidean = time.time() - start
 
-    print()
-    print()
+    accuracies[dataset] = (acc_sax, acc_euclidean)
+    times[dataset] = (time_sax, time_euclidean)
 
-
-# # Currently, I cannot append KNN to the pipeline, as it will raise problems
-# # Due to the fact that it will transform X_test to a distance matrix
-# # of (X_test.shape[0], X_test.shape[0])
-# accuracies = {}
-# for dataset, w in datasets:
-#     X_train, y_train, X_test, y_test = data_loader.load_dataset(dataset)
-#     pipe = Pipeline([
-#         (
-#             'transform', 
-#             SymbolicAggregateApproximation(n_segments=w, alphabet_size_avg=10)
-#         ),
-#         (
-#             'distance', 
-#             FunctionTransformer(calc_distances, validate=False, pass_y=False)
-#         )
-#     ])
-
-#     all_X = numpy.vstack((X_train, X_test))
-#     distances = pipe.transform(all_X)
-
-#     # We only need the distances to the timeseries from the training set.
-#     # Both for the test and the training set.
-#     X_train_dist = distances[:len(X_train), :len(X_train)]
-#     X_test_dist = distances[len(X_train):, :len(X_train)]
-
-#     knn = KNeighborsClassifier(n_neighbors=1, metric='precomputed')
-#     knn.fit(X_train_dist, y_train)
-#     acc_sax = accuracy_score(y_test, knn.predict(X_test_dist))
-
-#     knn = KNeighborsClassifier(n_neighbors=1, metric='euclidean')
-#     knn.fit(X_train.reshape(X_train.shape[:-1]), y_train)
-#     predictions = knn.predict(X_test.reshape(X_test.shape[:-1]))
-#     acc_euclidean = accuracy_score(y_test, predictions)
-
-#     accuracies[dataset] = (acc_sax, acc_euclidean)
-
-# print_table(accuracies)
+print_table(accuracies, times)
