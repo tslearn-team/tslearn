40d63d0376eb9e53b8447245698ee85986b6ced4
Gilles Vandewiele <givdwiel.vandewiele@ugent.be>
2020-03-08 11:48:16 +0100

    doc changes

--- tslearn/metrics.py
+++ tslearn/metrics.py
@@ -692,7 +692,7 @@ def _cdist_generic(dist_fun, dataset1, dataset2, n_jobs, verbose,
         The number of jobs to run in parallel.
         ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
         ``-1`` means using all processors. See scikit-learns'
-        `Glossary <https://scikit-learn.org/stable/glossary.html#term-n-jobs>`_
+        `Glossary <https://scikit-learn.org/stable/glossary.html#term-n-jobs>`__
         for more details.
 
     verbose : int, optional (default=0)
@@ -700,7 +700,7 @@ def _cdist_generic(dist_fun, dataset1, dataset2, n_jobs, verbose,
         Above 50, the output is sent to stdout.
         The frequency of the messages increases with the verbosity level.
         If it more than 10, all iterations are reported.
-        `Glossary <https://joblib.readthedocs.io/en/latest/parallel.html#parallel-reference-documentation>`_
+        `Glossary <https://joblib.readthedocs.io/en/latest/parallel.html#parallel-reference-documentation>`__
         for more details.
 
     compute_diagonal : bool (default: True)
@@ -802,7 +802,7 @@ def cdist_dtw(dataset1, dataset2=None, global_constraint=None,
         The number of jobs to run in parallel.
         ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
         ``-1`` means using all processors. See scikit-learns'
-        `Glossary <https://scikit-learn.org/stable/glossary.html#term-n-jobs>`_
+        `Glossary <https://scikit-learn.org/stable/glossary.html#term-n-jobs>`__
         for more details.
 
     verbose : int, optional (default=0)
@@ -810,7 +810,7 @@ def cdist_dtw(dataset1, dataset2=None, global_constraint=None,
         Above 50, the output is sent to stdout.
         The frequency of the messages increases with the verbosity level.
         If it more than 10, all iterations are reported.
-        `Glossary <https://joblib.readthedocs.io/en/latest/parallel.html#parallel-reference-documentation>`_
+        `Glossary <https://joblib.readthedocs.io/en/latest/parallel.html#parallel-reference-documentation>`__
         for more details.
 
     Returns
@@ -981,14 +981,14 @@ def cdist_gak(dataset1, dataset2=None, sigma=1., n_jobs=None, verbose=0):
         The number of jobs to run in parallel.
         ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
         ``-1`` means using all processors. See scikit-learns'
-        `Glossary <https://scikit-learn.org/stable/glossary.html#term-n-jobs>`_
+        `Glossary <https://scikit-learn.org/stable/glossary.html#term-n-jobs>`__
         for more details.
     verbose : int, optional (default=0)
         The verbosity level: if non zero, progress messages are printed. 
         Above 50, the output is sent to stdout. 
         The frequency of the messages increases with the verbosity level. 
         If it more than 10, all iterations are reported.
-        `Glossary <https://joblib.readthedocs.io/en/latest/parallel.html#parallel-reference-documentation>`_
+        `Glossary <https://joblib.readthedocs.io/en/latest/parallel.html#parallel-reference-documentation>`__
         for more details.
 
     Returns
@@ -1150,20 +1150,22 @@ def cdist_sax(dataset1, breakpoints_avg, size_fitted, dataset2=None,
     dataset1 : array-like
         A dataset of time series
 
+    breakpoints_avg : array-like
+        The breakpoints used to assign the alphabet symbols.
+
+    size_fitted: int
+        The original timesteps in the timeseries, before
+        discretizing through SAX.
+
     dataset2 : array-like (default: None)
         Another dataset of time series. If `None`, self-similarity of
         `dataset1` is returned.
-
-    breakpoints_avg: The breakpoints used to assign the alphabet symbols.
-
-    size_fitted: The original timesteps in the timeseries, before
-    discretizing through SAX.
 
     n_jobs : int or None, optional (default=None)
         The number of jobs to run in parallel.
         ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
         ``-1`` means using all processors. See scikit-learns'
-        `Glossary <https://scikit-learn.org/stable/glossary.html#term-n-jobs>`_
+        `Glossary <https://scikit-learn.org/stable/glossary.html#term-n-jobs>`__
         for more details.
 
     verbose : int, optional (default=0)
@@ -1171,7 +1173,7 @@ def cdist_sax(dataset1, breakpoints_avg, size_fitted, dataset2=None,
         Above 50, the output is sent to stdout.
         The frequency of the messages increases with the verbosity level.
         If it more than 10, all iterations are reported.
-        `Glossary <https://joblib.readthedocs.io/en/latest/parallel.html#parallel-reference-documentation>`_
+        `Glossary <https://joblib.readthedocs.io/en/latest/parallel.html#parallel-reference-documentation>`__
         for more details.
 
     Returns
