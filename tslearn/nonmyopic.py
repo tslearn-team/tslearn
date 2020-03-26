030587b6195ca63705cbf2ffa125edfa84d18eba
painblanc_f <francois-33>
2020-03-26 12:24:58 +0100

    Non mypopic EC tslearn

--- tslearn/nonmyopic.py
+++ tslearn/nonmyopic.py
@@ -7,18 +7,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 from sklearn.metrics import roc_auc_score
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--cost_time_parameter', type=float, default=0.01)
-args = parser.parse_args()
+import os.path
 
 
 class NonMyopicEarlyClassification(BaseEstimator, ClassifierMixin):
     """Early Classification modelling for time series using the model presented in [1]
 
     Parameters
-    ------------------
+    ----------
     number_cluster : int
         Number of clusters to form.
 
@@ -90,29 +86,29 @@ class NonMyopicEarlyClassification(BaseEstimator, ClassifierMixin):
         self.lamb = lamb
         self.cost_time_parameter = cost_time_parameter
 
-    def clustering(self, X_train):
+    def clustering(self, X):
         """
         This function compute and fit clusters for a training dataset of time series using K-means and euclidean
         distances
         Parameters
-        -----------------
-        X_train: array-like
+        ----------
+        X: array-like
             The training dataset of time series
 
         Returns
-        -----------------
+        -------
         Vector : a vector as long as the number of series in the training dataset that indicates in which cluster the
         series at the same index belongs
 
          Float : the silhouette score of the fitting of the clustering
         """
         self.cluster_ = TimeSeriesKMeans(n_clusters=self.number_cluster)
-        c_k = self.cluster_.fit_predict((to_time_series_dataset(X_train)))
-        silhouette = silhouette_score(to_time_series_dataset(X_train), c_k, metric="euclidean")
+        c_k = self.cluster_.fit_predict((to_time_series_dataset(X)))
+        silhouette = silhouette_score(to_time_series_dataset(X), c_k, metric="euclidean")
 
         return c_k, silhouette
 
-    def fit(self, X_train, Y_train):
+    def fit(self, X, Y):
         """
         This function fits classifiers that are currently multilayer perceptrons to a training set of time series and
         associated classes. A classifier is fit for each time stamp above a minimum time stamp that is an attribute of
@@ -120,56 +116,55 @@ class NonMyopicEarlyClassification(BaseEstimator, ClassifierMixin):
         This function should be divided among 2 or 3 functions.
 
         Parameters
-        ---------------
-        X_train: Array-like
+        ----------
+        X: Array-like
             a dataset of time series
-        Y_train: vector
+        Y: vector
             the associated classes of the series from X_train
         """
-        classes_y = np.unique(Y_train)
-        c_k, silhouette = self.clustering(X_train)
+        classes_y = np.unique(Y)
+        c_k, silhouette = self.clustering(X)
         self.silhouette_ = silhouette
         self.classifier_ = []
         self.clusters_ = {}
         self.pyhatyck_ = {}
         self.indice_ck_ = []
         self.number_classes_ = len(classes_y)
-        self.len_X_ = X_train.shape[1]
-        mid_X = int(X_train.shape[0] / 2)
-        X_train1 = X_train[:mid_X]
-        Y_train1 = Y_train[:mid_X]
-        X_train2 = X_train[mid_X:]
-        Y_train2 = Y_train[mid_X:]
+        self.len_X_ = X.shape[1]
+        mid_X = int(X.shape[0] / 2)
+        X1 = X[:mid_X]
+        Y1 = Y[:mid_X]
+        X2 = X[mid_X:]
+        Y2 = Y[mid_X:]
         c_k2 = c_k[mid_X:]
         self.pyck_ = np.zeros(shape=(self.number_classes_, self.number_cluster))
         for current_cluster in range(0, self.number_cluster):
-            #Maybe shall right indice_ck_cm rather than ck_cm, dunno what ck_cm is
             self.clusters_["ck_cm{0}".format(current_cluster)] = []
             self.pyhatyck_["pyhatycks{0}".format(current_cluster)] = []
             self.indice_ck_.append(np.where(c_k == current_cluster))
-            Y_train_current_cluster = Y_train[self.indice_ck_[-1]]
+            Y_current_cluster = Y[self.indice_ck_[-1]]
             for current_class in range(0, self.number_classes_):
                 value_current_class = classes_y[current_class]
-                if len(Y_train_current_cluster) == 0:
+                if len(Y_current_cluster) == 0:
                     current_pyck = 0
                 else:
-                    current_pyck = len(Y_train_current_cluster[Y_train_current_cluster == value_current_class]) / \
-                                   len(Y_train_current_cluster)
+                    current_pyck = len(Y_current_cluster[Y_current_cluster == value_current_class]) / \
+                                   len(Y_current_cluster)
                 self.pyck_[current_class][current_cluster] = current_pyck
 
-        for i in range(self.minimum_time_stamp, X_train.shape[1] + 1):
+        for i in range(self.minimum_time_stamp, X.shape[1] + 1):
             self.classifier_.append(MLPClassifier(solver=self.solver, hidden_layer_sizes=self.hidden_layer_sizes,
                                                   random_state=self.random_state,
                                                   max_iter=self.maximum_iteration))
-            self.classifier_[-1].fit(X_train1[:, :i], Y_train1)
+            self.classifier_[-1].fit(X1[:, :i], Y1)
             for current_cluster in range(0, self.number_cluster):
                 index_cluster = np.where(c_k2 == current_cluster)
                 if len(index_cluster[0]) != 0:
-                    X_train2_current_cluster = np.squeeze(X_train2[index_cluster, :i], axis=0)
-                    Y_train2_current_cluster = Y_train2[tuple(index_cluster)]
-                    Y_train2_hat = self.classifier_[-1].predict(X_train2_current_cluster[:, :i])
-                    self.clusters_["ck_cm{0}".format(current_cluster)].append(confusion_matrix(Y_train2_current_cluster,
-                                                                                               Y_train2_hat,
+                    X2_current_cluster = np.squeeze(X2[index_cluster, :i], axis=0)
+                    Y2_current_cluster = Y2[tuple(index_cluster)]
+                    Y2_hat = self.classifier_[-1].predict(X2_current_cluster[:, :i])
+                    self.clusters_["ck_cm{0}".format(current_cluster)].append(confusion_matrix(Y2_current_cluster,
+                                                                                               Y2_hat,
                                                                                                labels=classes_y))
                     column_sum = self.clusters_["ck_cm{0}".format(current_cluster)][-1].sum(1)
                     for current_class in range(0, self.number_classes_):
@@ -182,17 +177,17 @@ class NonMyopicEarlyClassification(BaseEstimator, ClassifierMixin):
                                      self.clusters_["ck_cm{0}".format(current_cluster)][-1][1][1] / column_sum[1]]])
                     self.pyhatyck_["pyhatycks{0}".format(current_cluster)].append(matrix_proba_current_cluster_class)
 
-    def get_avg_dist(self, truncated_xt):
+    def get_avg_dist(self, X):
         """
         Compute the average euclidean distance of a given time series to the centroid of each cluster of the model
 
         Parameters
-        --------------------
-        xt: Vector
-            A time series that may  be incomplete
+        ----------
+        X: Vector
+            A time series that may  be truncated
 
         Returns
-        -------------------
+        -------
         Float : the average euclidean distance of the time series to the centroid of each cluster of the model
         Float : the minimum distance between the centroid of a cluster and the time series
         """
@@ -200,50 +195,50 @@ class NonMyopicEarlyClassification(BaseEstimator, ClassifierMixin):
         sum_distances = 0
         minimum_distance = 0
         for current_cluster in range(0, self.number_cluster):
-            euclidean_distance = np.linalg.norm(truncated_xt - self.cluster_.cluster_centers_[current_cluster, :len(truncated_xt)])
+            euclidean_distance = np.linalg.norm(X - self.cluster_.cluster_centers_[current_cluster, :len(X)])
             sum_distances = sum_distances + euclidean_distance
             if minimum_distance < euclidean_distance:
                 minimum_distance = euclidean_distance
         return sum_distances / self.number_cluster, minimum_distance
 
-    def get_dist(self, truncated_xt, wanted_cluster):
+    def get_dist(self, X, wanted_cluster):
         """
         Compute the euclidean distance between a time series xt and the centroid of a cluster of the model
         Parameters
-        ----------------
-        xt: vector
-            a time series that may be incomplete
+        ----------
+        X: vector
+            a time series that may be truncated
         wanted_cluster: int
             the number of the cluster for which the distance is wanted
 
         Returns
-        ----------------
+        -------
         Float: the expected euclidean distance
         """
 
-        euclidean_dist = np.linalg.norm(truncated_xt - self.cluster_.cluster_centers_[wanted_cluster, :len(truncated_xt)])
+        euclidean_dist = np.linalg.norm(X - self.cluster_.cluster_centers_[wanted_cluster, :len(X)])
 
         return euclidean_dist
 
-    def get_cluster_prob(self, truncated_xt):
+    def get_cluster_prob(self, X):
         """
         This function computes the probabilities of the time series xt to be in a cluster of the model
 
         Parameters
-        -------------------
-        xt: vector
-            a time series that may be incomplete
+        ----------
+        X: vector
+            a time series that may be truncated
 
         Returns
-        ----------------
+        -------
         vector : the probabilities of the given time series to be in the clusters of the model
         """
-        average_distance, minimum_distance = self.get_avg_dist(truncated_xt)
+        average_distance, minimum_distance = self.get_avg_dist(X)
         minimum_distance_to_average = (average_distance - minimum_distance) / average_distance
         sum_sk = 0
         sk = []
         for current_cluster in range(0, self.number_cluster):
-            d_k = self.get_dist(truncated_xt, current_cluster)
+            d_k = self.get_dist(X, current_cluster)
             Delta_k = (average_distance - d_k) / average_distance
             proba_current_cluster = 1 / (math.exp(self.lamb * minimum_distance_to_average) + math.exp(self.lamb * (minimum_distance_to_average - Delta_k)) + 10e-6)
             sk.append(proba_current_cluster)
@@ -251,25 +246,25 @@ class NonMyopicEarlyClassification(BaseEstimator, ClassifierMixin):
         final_sk = [(x / sum_sk) for x in sk]
         return final_sk
 
-    def cost_function(self, truncated_xt, tau):
+    def cost_function(self, X, tau):
         """
         From a incomplete series xt, this function compute the expected cost of a prediction made at time "last time of
         xt + tau"
 
         Parameters
-        ------------------
-        xt: vector
-            a time series that may be incomplete for which we want to have the cost
+        ----------
+        X: vector
+            a time series that may be truncated for which we want to have the cost
         tau: int
             gives the future time for which the cost is calculated
 
         Returns
-        -----------------------
+        -------
         float : the computed cost
         """
         cost = 0
-        proba_clusters = self.get_cluster_prob(truncated_xt=truncated_xt)
-        truncated_t = truncated_xt.shape[-1]
+        proba_clusters = self.get_cluster_prob(X=X)
+        truncated_t = X.shape[-1]
         for current_cluster in range(0, self.number_cluster):
             cost_y = 0
             for y in range(0, self.number_classes_):
@@ -289,7 +284,7 @@ class NonMyopicEarlyClassification(BaseEstimator, ClassifierMixin):
         """
         We want to minimize a function "funct" according a time series "xt" for a list of integers from 0 to "stop"
         Parameters
-        ---------------------
+        ----------
         end_of_time: int
             the highest integer at which the function to be minimized is calculated
         function_to_minimize: funct
@@ -298,7 +293,7 @@ class NonMyopicEarlyClassification(BaseEstimator, ClassifierMixin):
             the time series related to the function "funct"
 
         Returns
-         ---------------------
+         ------
          int : The integer in {0,...,stop} that minimizes "funct"
          float : the so fat minimum cost
         """
@@ -316,13 +311,13 @@ class NonMyopicEarlyClassification(BaseEstimator, ClassifierMixin):
         This function classifies a single time series xt
 
         Parameters
-        ----------------------
+        ----------
         xt: vector
             a time series that is probably incomplete but that nonetheless we want to classify
         cost_function: funct
             The cost function that will be use to achieve our classification purpose
         Returns
-        -----------------------
+        -------
         int: the class which is predicted
         int : the time of the prediction
         float : the probability used for computing the cost
@@ -370,64 +365,64 @@ class NonMyopicEarlyClassification(BaseEstimator, ClassifierMixin):
                 good_prediction = good_prediction + 1
         return good_prediction / len(Y_test)
 
-    def predict(self, X_test):
+    def predict(self, X):
         """
         Predicts the classes of the series of the test dataset.
 
         Parameters
-        ---------------------
+        ----------
         X_test : Array-like
             The test dataset
 
         Returns
-        ----------------------
+        -------
         Vector : the predicted classes
         """
 
-        Y_pred_test = []
+        Y_pred = []
 
-        for t in range(0, X_test.shape[0]):
-            new_classif, new_time, new_proba, new_cost = self.classification(X_test[t], self.cost_function)
-            Y_pred_test.append(new_classif)
-        return Y_pred_test
+        for t in range(0, X.shape[0]):
+            new_classif, new_time, new_proba, new_cost = self.classification(X[t], self.cost_function)
+            Y_pred.append(new_classif)
+        return Y_pred
 
-    def predict_completed(self, X_test, Y_test):
+    def predict_completed(self, X, Y):
         """
         Predicts the classes of the series of the test dataset and has also access the the true classes to establish
         comparisons
 
         Parameters
-        ---------------------
-        X_test: Array-like
+        ----------
+        X: Array-like
             The data set that needs to be predict
-        Y_test : Array-vector
+        Y: Array-vector
             The true classes
 
         Returns
-        ------------------
+        -------
         float : average time of prediction
         float : variance of the time of prediction
         float : AUC
         float : average cost
         """
-        Y_pred_test = []
+        Y_pred = []
         time_predictions = []
         pred_proba = []
         cost_average = 0
 
-        for current_serie in range(0, X_test.shape[0]):
-            new_classif, new_time, new_proba, new_cost = self.classification(X_test[current_serie], self.cost_function)
-            Y_pred_test.append(new_classif)
+        for current_serie in range(0, X.shape[0]):
+            new_classif, new_time, new_proba, new_cost = self.classification(X[current_serie], self.cost_function)
+            Y_pred.append(new_classif)
             time_predictions.append(new_time)
             pred_proba.append(new_proba)
             cost_average = cost_average + new_cost
         average_pred, variance_pred = self.avg_prediction(time_predictions)
-        pred_proba = np.reshape(np.asarray(pred_proba), (X_test.shape[0], 2))
-        auc = roc_auc_score(Y_test, pred_proba[:, 1])
-        cost_average = cost_average / X_test.shape[0]
+        pred_proba = np.reshape(np.asarray(pred_proba), (X.shape[0], 2))
+        auc = roc_auc_score(Y, pred_proba[:, 1])
+        cost_average = cost_average / X.shape[0]
         return average_pred, variance_pred, auc, cost_average
 
-    def graphic(self, PATH, name, prediction_time=None):
+    def graphic(self, path, name, prediction_time=None):
         plt.clf()
         for current_cluster in range(0, self.number_cluster):
             for current_class in range(0, self.number_classes_):
@@ -435,13 +430,14 @@ class NonMyopicEarlyClassification(BaseEstimator, ClassifierMixin):
                     if current_class != current_hat_class:
                         sum_prob = [self.pyck_[current_class][current_cluster] * item[current_class][current_hat_class]
                                     for item in self.pyhatyck_["pyhatycks{0}".format(current_cluster)]]
-                        lab = \
-                            "y=" + str(current_class) + "yhat=" + str(current_hat_class) + "ck=" + str(current_cluster)
+                        lab = "y={} yhat={} ck={}".format(
+                            current_class, current_hat_class, current_cluster
+                        )
                         plt.plot(sum_prob, label=lab)
         plt.legend()
         if prediction_time is not None:
             plt.title("The prediction time is " + str(prediction_time))
-        plt.savefig(PATH + name + ".png", dpi=500)
+        plt.savefig(os.path.join(path, name + ".png"), dpi=500)
 
     def cost_time(self, t):
         time_stamp = np.linspace(0, self.len_X_, num=self.len_X_)
