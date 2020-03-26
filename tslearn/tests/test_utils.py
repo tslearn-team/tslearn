d378ccd8f450284e2dbc22af8990414092b2b038
Roman Yurchak <rth.yurchak@gmail.com>
2020-03-26 10:25:25 +0100

    Split out conversion test depending on missing dependencies

--- tslearn/tests/test_utils.py
+++ tslearn/tests/test_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pickle
 import os
+from numpy.testing import assert_allclose
 
 import pytest
 
@@ -11,9 +12,8 @@ __author__ = 'Romain Tavenard romain.tavenard[at]univ-rennes2.fr'
 
 def test_arraylike_copy():
     X_npy = np.array([1, 2, 3])
-    np.testing.assert_allclose(tslearn.utils._arraylike_copy(X_npy), X_npy)
-    np.testing.assert_allclose(tslearn.utils._arraylike_copy(X_npy) is X_npy,
-                               False)
+    assert_allclose(tslearn.utils._arraylike_copy(X_npy), X_npy)
+    assert_allclose(tslearn.utils._arraylike_copy(X_npy) is X_npy, False)
 
 
 def test_save_load():
@@ -23,15 +23,15 @@ def test_save_load():
     tslearn.utils.save_timeseries_txt("tmp-tslearn-test.txt", dataset)
     reloaded_dataset = tslearn.utils.load_timeseries_txt(
         "tmp-tslearn-test.txt")
-    np.testing.assert_allclose(dataset, reloaded_dataset)
+    assert_allclose(dataset, reloaded_dataset)
 
     dataset = tslearn.utils.to_time_series_dataset([[1, 2, 3, 4], [1, 2, 3]])
     tslearn.utils.save_timeseries_txt("tmp-tslearn-test.txt", dataset)
     reloaded_dataset = tslearn.utils.load_timeseries_txt(
         "tmp-tslearn-test.txt")
     for ts0, ts1 in zip(dataset, reloaded_dataset):
-        np.testing.assert_allclose(ts0[:tslearn.utils.ts_size(ts0)],
-                                   ts1[:tslearn.utils.ts_size(ts1)])
+        assert_allclose(ts0[:tslearn.utils.ts_size(ts0)],
+                        ts1[:tslearn.utils.ts_size(ts1)])
 
 
 def test_label_categorizer():
@@ -44,52 +44,50 @@ def test_label_categorizer():
     y_tr = lc2.inverse_transform([[0, 1, 0], [0, 0, 1], [1, 0, 0]])
     ref = np.array([1., 2., -1.])
 
-    np.testing.assert_allclose(ref, y_tr)
+    assert_allclose(ref, y_tr)
 
 
 def test_conversions():
-    pytest.importorskip('pandas')
     n, sz, d = 15, 10, 3
     rng = np.random.RandomState(0)
     tslearn_dataset = rng.randn(n, sz, d)
 
-    np.testing.assert_allclose(
+    assert_allclose(
         tslearn_dataset,
         tslearn.utils.from_pyts_dataset(
             tslearn.utils.to_pyts_dataset(tslearn_dataset)
         )
     )
 
-    np.testing.assert_allclose(
-        tslearn_dataset,
-        tslearn.utils.from_sktime_dataset(
-            tslearn.utils.to_sktime_dataset(tslearn_dataset)
-        )
-    )
-
-    np.testing.assert_allclose(
+    assert_allclose(
         tslearn_dataset,
         tslearn.utils.from_seglearn_dataset(
             tslearn.utils.to_seglearn_dataset(tslearn_dataset)
         )
     )
 
-    np.testing.assert_allclose(
+    assert_allclose(
         tslearn_dataset,
         tslearn.utils.from_stumpy_dataset(
             tslearn.utils.to_stumpy_dataset(tslearn_dataset)
         )
     )
 
-    if os.environ.get("DO_NOT_TEST_CESIUM_CAST", None) is None:
-        np.testing.assert_allclose(
-            tslearn_dataset,
-            tslearn.utils.from_cesium_dataset(
-                tslearn.utils.to_cesium_dataset(tslearn_dataset)
-            )
-        )
 
-    np.testing.assert_allclose(
+def test_conversions_with_pandas():
+    pytest.importorskip('pandas')
+    n, sz, d = 15, 10, 3
+    rng = np.random.RandomState(0)
+    tslearn_dataset = rng.randn(n, sz, d)
+
+    assert_allclose(
+        tslearn_dataset,
+        tslearn.utils.from_sktime_dataset(
+            tslearn.utils.to_sktime_dataset(tslearn_dataset)
+        )
+    )
+
+    assert_allclose(
         tslearn_dataset,
         tslearn.utils.from_tsfresh_dataset(
             tslearn.utils.to_tsfresh_dataset(tslearn_dataset)
@@ -97,9 +95,22 @@ def test_conversions():
     )
 
     tslearn_dataset = rng.randn(1, sz, d)
-    np.testing.assert_allclose(
+    assert_allclose(
         tslearn_dataset,
         tslearn.utils.from_pyflux_dataset(
             tslearn.utils.to_pyflux_dataset(tslearn_dataset)
+        )
+    )
+
+def test_conversions_cesium():
+    pytest.importorskip('cesium')
+    n, sz, d = 15, 10, 3
+    rng = np.random.RandomState(0)
+    tslearn_dataset = rng.randn(n, sz, d)
+
+    assert_allclose(
+        tslearn_dataset,
+        tslearn.utils.from_cesium_dataset(
+            tslearn.utils.to_cesium_dataset(tslearn_dataset)
         )
     )
