2d6a85967a914c5f394be83c1caa09d573fa7548
Roman Yurchak <rth.yurchak@gmail.com>
2020-03-26 11:23:29 +0100

    Lint

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 def pytest_collection_modifyitems(config, items):
     try:
         import pandas
@@ -15,23 +16,21 @@ def pytest_collection_modifyitems(config, items):
         skip_marker = pytest.mark.skip(reason="pandas not installed!")
         for item in items:
             if item.name in [
-                    "tslearn.utils.from_tsfresh_dataset",
-                    "tslearn.utils.to_tsfresh_dataset",
-                    "tslearn.utils.from_sktime_dataset",
-                    "tslearn.utils.to_sktime_dataset",
-                    "tslearn.utils.from_pyflux_dataset",
-                    "tslearn.utils.to_pyflux_dataset",
-                    "tslearn.utils.from_cesium_dataset",
-                    "tslearn.utils.to_cesium_dataset",
+                "tslearn.utils.from_tsfresh_dataset",
+                "tslearn.utils.to_tsfresh_dataset",
+                "tslearn.utils.from_sktime_dataset",
+                "tslearn.utils.to_sktime_dataset",
+                "tslearn.utils.from_pyflux_dataset",
+                "tslearn.utils.to_pyflux_dataset",
+                "tslearn.utils.from_cesium_dataset",
+                "tslearn.utils.to_cesium_dataset",
             ]:
                 item.add_marker(skip_marker)
     elif cesium is None:
         skip_marker = pytest.mark.skip(reason="cesium not installed!")
         for item in items:
             if item.name in [
-                    "tslearn.utils.to_cesium_dataset",
-                    "tslearn.utils.from_cesium_dataset",
+                "tslearn.utils.to_cesium_dataset",
+                "tslearn.utils.from_cesium_dataset",
             ]:
                 item.add_marker(skip_marker)
-
-
