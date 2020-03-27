d5b0234cca1bbab1a0bd20091df44380aca71ee8
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2020-03-27 18:42:33 +0100

    Change test name and test SVR too

--- tslearn/tests/test_svm.py
+++ tslearn/tests/test_svm.py
@@ -1,22 +1,24 @@
 import numpy as np
 
 from tslearn.metrics import cdist_gak
-from tslearn.svm import TimeSeriesSVC
+from tslearn.svm import TimeSeriesSVC, TimeSeriesSVR
 
 __author__ = 'Romain Tavenard romain.tavenard[at]univ-rennes2.fr'
 
 
-def test_svm_gak():
-    n, sz, d = 15, 10, 3
+def test_gamma_value_svm():
+    n, sz, d = 5, 10, 3
     rng = np.random.RandomState(0)
     time_series = rng.randn(n, sz, d)
     labels = rng.randint(low=0, high=2, size=n)
 
     gamma = 10.
-    gak_km = TimeSeriesSVC(kernel="gak", gamma=gamma)
-    sklearn_X, _ = gak_km._preprocess_sklearn(time_series, labels,
-                                              fit_time=True)
+    for ModelClass in [TimeSeriesSVC, TimeSeriesSVR]:
+        gak_model = ModelClass(kernel="gak", gamma=gamma)
+        sklearn_X, _ = gak_model._preprocess_sklearn(time_series,
+                                                     labels,
+                                                     fit_time=True)
 
-    cdist_mat = cdist_gak(time_series, sigma=np.sqrt(gamma / 2.))
+        cdist_mat = cdist_gak(time_series, sigma=np.sqrt(gamma / 2.))
 
-    np.testing.assert_allclose(sklearn_X, cdist_mat)
+        np.testing.assert_allclose(sklearn_X, cdist_mat)
