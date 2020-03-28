9d86ccfcc6e6e0ea627d04656c524c3df7665709
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2020-03-28 10:23:54 +0100

    kmeans example detailed + note on preprocessing

--- tslearn/docs/examples/plot_kmeans.py
+++ tslearn/docs/examples/plot_kmeans.py
@@ -5,9 +5,35 @@ k-means
 
 This example uses :math:`k`-means clustering for time series. Three variants of
 the algorithm are available: standard
-Euclidean :math:`k`-means, DBA-:math:`k`-means (for DTW Barycenter Averaging)
-and Soft-DTW :math:`k`-means.
+Euclidean :math:`k`-means, DBA-:math:`k`-means (for DTW Barycenter
+Averaging [1])
+and Soft-DTW :math:`k`-means [2].
 
+In the figure below, each row corresponds to the result of a different
+clustering. In a row, each sub-figure corresponds to a cluster.
+It represents the set
+of time series from the training set that were assigned to the considered
+cluster (in black) as well as the barycenter of the cluster (in red).
+
+A note on pre-processing
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+In this example, time series are preprocessed using
+`TimeSeriesScalerMeanVariance`. This scaler is such that each output time
+series has zero mean and unit variance.
+The assumption here is that the range of a given time series is uninformative
+and one only wants to compare shapes in an amplitude-invariant manner (when
+time series are multivariate, this also rescales all modalities such that there
+will not be a single modality responsible for a large part of the variance).
+This means that one cannot scale barycenters back to data range because each
+time series is scaled independently and there is hence no such thing as an
+overall data range.
+
+[1] F. Petitjean, A. Ketterlin & P. Gancarski. A global averaging method \
+for dynamic time warping, with applications to clustering. Pattern \
+Recognition, Elsevier, 2011, Vol. 44, Num. 3, pp. 678-693
+[2] M. Cuturi, M. Blondel "Soft-DTW: a Differentiable Loss Function for \
+Time-Series," ICML 2017.
 """
 
 # Author: Romain Tavenard
