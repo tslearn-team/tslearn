4d3d576c07457e3022ce16ff72fbceb7b12eafe4
Gilles Vandewiele <givdwiel.vandewiele@ugent.be>
2020-03-29 08:56:13 +0200

    updated three examples

--- tslearn/docs/examples/plot_shapelet_locations.py
+++ tslearn/docs/examples/plot_shapelet_locations.py
@@ -14,7 +14,6 @@ http://fs.ismll.de/publicspace/LearningShapelets/.
 # License: BSD 3 clause
 
 import numpy
-from keras.optimizers import Adagrad
 import matplotlib.pyplot as plt
 
 from tslearn.datasets import CachedDatasets
@@ -22,11 +21,17 @@ from tslearn.preprocessing import TimeSeriesScalerMinMax
 from tslearn.shapelets import ShapeletModel, \
     grabocka_params_to_shapelet_size_dict
 
-numpy.random.seed(0)
+# Set a seed to ensure determinism
+numpy.random.seed(42)
+
+# Load the Trace dataset
 X_train, y_train, X_test, y_test = CachedDatasets().load_dataset("Trace")
+
+# Normalize each of the timeseries in the Trace dataset
 X_train = TimeSeriesScalerMinMax().fit_transform(X_train)
 X_test = TimeSeriesScalerMinMax().fit_transform(X_test)
 
+# Get statistics of the dataset
 n_ts, ts_sz = X_train.shape[:2]
 n_classes = len(set(y_train))
 
@@ -34,23 +39,33 @@ n_classes = len(set(y_train))
 shapelet_sizes = grabocka_params_to_shapelet_size_dict(n_ts=n_ts,
                                                        ts_sz=ts_sz,
                                                        n_classes=n_classes,
-                                                       l=0.1,
-                                                       r=2)
+                                                       l=0.125,
+                                                       r=1)
 
+# Define the model and fit it using the training data
 shp_clf = ShapeletModel(n_shapelets_per_size=shapelet_sizes,
-                        optimizer=Adagrad(lr=.1),
                         weight_regularizer=.01,
-                        max_iter=50,
-                        verbose=0)
+                        max_iter=100,
+                        verbose=0,
+                        random_state=42)
 shp_clf.fit(X_train, y_train)
-predicted_locations = shp_clf.locate(X_test)
 
-test_ts_id = 0
-plt.figure()
+# Get the number of extracted shapelets, the (minimal) distances from
+# each of the timeseries to each of the shapelets, and the corresponding
+# locations (index) where the minimal distance was found
 n_shapelets = sum(shapelet_sizes.values())
+distances = shp_clf.transform(X_train)
+predicted_locations = shp_clf.locate(X_train)
+
+plt.figure()
 plt.title("Example locations of shapelet matches "
           "(%d shapelets extracted)".format(n_shapelets))
-plt.plot(X_test[test_ts_id].ravel())
+
+# Plot the test timeseries with the best matches with the shapelets
+test_ts_id = numpy.argmin(numpy.sum(distances, axis=1))
+plt.plot(X_train[test_ts_id].ravel())
+
+# Plot the shapelets on their best-matching locations
 for idx_shp, shp in enumerate(shp_clf.shapelets_):
     t0 = predicted_locations[test_ts_id, idx_shp]
     plt.plot(numpy.arange(t0, t0 + len(shp)), shp, linewidth=2)
--- tslearn/docs/examples/plot_shapelets.py
+++ tslearn/docs/examples/plot_shapelets.py
@@ -3,8 +3,8 @@
 Learning Shapelets
 ==================
 
-This example illustrates the use of the "Learning Shapelets" method for a time
-series classification task.
+This example illustrates the use of the "Learning Shapelets" method for a
+time series classification task.
 
 More information on the method can be found at:
 http://fs.ismll.de/publicspace/LearningShapelets/.
@@ -24,11 +24,17 @@ from tslearn.shapelets import ShapeletModel, \
     grabocka_params_to_shapelet_size_dict
 from tslearn.utils import ts_size
 
+# Set seed for determinism
 numpy.random.seed(0)
+
+# Load the Trace dataset
 X_train, y_train, X_test, y_test = CachedDatasets().load_dataset("Trace")
+
+# Normalize each of the timeseries in the Trace dataset
 X_train = TimeSeriesScalerMinMax().fit_transform(X_train)
 X_test = TimeSeriesScalerMinMax().fit_transform(X_test)
 
+# Get statistics of the dataset
 n_ts, ts_sz = X_train.shape[:2]
 n_classes = len(set(y_train))
 
@@ -37,19 +43,24 @@ shapelet_sizes = grabocka_params_to_shapelet_size_dict(n_ts=n_ts,
                                                        ts_sz=ts_sz,
                                                        n_classes=n_classes,
                                                        l=0.1,
-                                                       r=2)
+                                                       r=1)
 
-# Define the model using parameters provided by the authors (except that we use
-# fewer iterations here)
+# Define the model using parameters provided by the authors (except that we
+# use fewer iterations here)
 shp_clf = ShapeletModel(n_shapelets_per_size=shapelet_sizes,
                         optimizer=Adagrad(lr=.1),
+                        batch_size=16,
                         weight_regularizer=.01,
                         max_iter=200,
+                        random_state=42,
                         verbose=0)
 shp_clf.fit(X_train, y_train)
-predicted_labels = shp_clf.predict(X_test)
-print("Correct classification rate:", accuracy_score(y_test, predicted_labels))
 
+# Make predictions and calculate accuracy score
+pred_labels = shp_clf.predict(X_test)
+print("Correct classification rate:", accuracy_score(y_test, pred_labels))
+
+# Plot the different discovered shapelets
 plt.figure()
 for i, sz in enumerate(shapelet_sizes.keys()):
     plt.subplot(len(shapelet_sizes), 1, i + 1)
@@ -62,10 +73,9 @@ for i, sz in enumerate(shapelet_sizes.keys()):
 plt.tight_layout()
 plt.show()
 
-# The loss history is accessible via the `model` attribute that is a keras
-# model
+# The loss history is accessible via the `model` that is a keras model
 plt.figure()
-plt.plot(numpy.arange(1, 201), shp_clf.model.history.history["loss"])
+plt.plot(numpy.arange(1, 201), shp_clf.model_.history.history["loss"])
 plt.title("Evolution of cross-entropy loss during training")
 plt.xlabel("Epochs")
 plt.show()
