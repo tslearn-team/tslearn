9fdaced99524cfb70b59eb59cd08d6666e7b4b80
Johann Faouzi <johann.faouzi@gmail.com>
2020-04-03 14:29:22 +0200

    Revert change

--- tslearn/neighbors.py
+++ tslearn/neighbors.py
@@ -7,11 +7,7 @@ import numpy
 from sklearn import neighbors
 from sklearn.neighbors import (KNeighborsClassifier, NearestNeighbors,
                                KNeighborsRegressor)
-try:
-    from sklearn.neighbors._base import KNeighborsMixin
-except ImportError:
-    from sklearn.neighbors.base import KNeighborsMixin
-
+from sklearn.neighbors.base import KNeighborsMixin
 from sklearn.utils import check_array
 from sklearn.utils.validation import check_is_fitted
 from scipy.spatial.distance import cdist as scipy_cdist
