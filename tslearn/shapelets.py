603cbf410f91b5ab1a3c03dcc8c1fa8a9505ce8c
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2020-05-25 00:42:07 +0200

    Shapelets: model_ is callable but should be saved

--- tslearn/shapelets.py
+++ tslearn/shapelets.py
@@ -734,7 +734,7 @@ class ShapeletModel(ClassifierMixin, TransformerMixin,
     def _get_model_params(self):
         """Get model parameters that are sufficient to recapitulate it."""
         params = super()._get_model_params()
-        params.update({"_X_fit_dims": self._X_fit_dims})
+        params.update({"_X_fit_dims": self._X_fit_dims, "model_": self.model_})
         return params
 
     @staticmethod
