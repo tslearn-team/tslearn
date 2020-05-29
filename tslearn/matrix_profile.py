0747b35d3a0c59ce96dd57daed856721b6f67076
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2020-05-29 18:21:36 +0200

    Better docstrings + return sequences

--- tslearn/matrix_profile.py
+++ tslearn/matrix_profile.py
@@ -20,16 +20,37 @@ __author__ = 'Romain Tavenard romain.tavenard[at]univ-rennes2.fr'
 class MatrixProfile(TransformerMixin,
                     BaseModelPackage,
                     TimeSeriesBaseEstimator):
-    """TODO
+    """Matrix Profile transformation.
+
+    Matrix Profile was originally presented in [1]_.
+
+    Parameters
+    ----------
+    subsequence_length : int (default: 1)
+        Length of the subseries (also called window size) to be used for 
+        subseries distance computations.
+    
+    scale: bool (default: False)
+         Whether input data should be scaled for each feature of each time 
+         series to have zero mean and unit variance.
+         Default for this parameter is set to `True` to match the standard 
+         matrix profile setup.
     
     Examples
     --------
     >>> time_series = [0., 1., 3., 2., 9., 1., 14., 15., 1., 2., 2., 10., 7.]
     >>> ds = [time_series]
     >>> mp = MatrixProfile(subsequence_length=4, scale=False)
-    >>> mp.fit_transform(ds)  # doctest: +NORMALIZE_WHITESPACE +ELLIPSIS
-    array([[ 6.85...,  1.41...,  6.16..., 7.93..., 11.40...,
-            13.56..., 14.07..., 13.96..., 1.41...,  6.16...]])
+    >>> mp.fit_transform(ds)[0, :, 0]  # doctest: +ELLIPSIS
+    array([ 6.85...,  1.41...,  6.16...,  7.93..., 11.40...,
+           13.56..., 14.07..., 13.96...,  1.41...,  6.16...])
+
+    References
+    ----------
+    .. [1] C. M. Yeh, Y. Zhu, L. Ulanova, N.Begum et al. 
+       Matrix Profile I: All Pairs Similarity Joins for Time Series: A 
+       Unifying View that Includes Motifs, Discords and Shapelets.
+       ICDM 2016.
     """
 
     def __init__(self, subsequence_length=1, scale=True):
@@ -40,14 +61,24 @@ class MatrixProfile(TransformerMixin,
         return True
 
     def fit(self, X, y=None):
-        """TODO
+        """Fit a Matrix Profile representation.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_ts, sz, d)
+            Time series dataset
+
+        Returns
+        -------
+        PiecewiseAggregateApproximation
+            self
         """
         return self
 
     def _transform(self, X, y=None):
         n_ts, sz, d = X.shape
-        output_dim = sz - self.subsequence_length + 1
-        X_transformed = numpy.empty((n_ts, output_dim))
+        output_size = sz - self.subsequence_length + 1
+        X_transformed = numpy.empty((n_ts, output_size, 1))
         scaler = TimeSeriesScalerMeanVariance()
         for i_ts in range(n_ts):
             Xi = X[i_ts]
@@ -64,11 +95,12 @@ class MatrixProfile(TransformerMixin,
             segments_2d = segments.reshape((-1, self.subsequence_length * d))
             dists = squareform(pdist(segments_2d, "euclidean"))
             numpy.fill_diagonal(dists, numpy.inf)
-            X_transformed[i_ts] = dists.min(axis=1)
+            X_transformed[i_ts] = dists.min(axis=1, keepdims=True)
         return X_transformed
 
     def transform(self, X, y=None):
-        """TODO
+        """Transform a dataset of time series into its Matrix Profile
+         representation.
 
         Parameters
         ----------
@@ -77,16 +109,18 @@ class MatrixProfile(TransformerMixin,
 
         Returns
         -------
-        numpy.ndarray of shape (n_ts, output_dim)
-            PAA-Transformed dataset
+        numpy.ndarray of shape (n_ts, output_size, 1)
+            Matrix-Profile-Transformed dataset. `ouput_size` is equal to 
+            `sz - subsequence_length + 1`
         """
         self._is_fitted()
-        X = check_array(X, allow_nd=True)
+        X = check_array(X, allow_nd=True, force_all_finite=False)
         X = check_dims(X)
         return self._transform(X, y)
 
     def fit_transform(self, X, y=None, **fit_params):
-        """TODO
+        """Transform a dataset of time series into its Matrix Profile
+         representation.
 
         Parameters
         ----------
@@ -95,9 +129,11 @@ class MatrixProfile(TransformerMixin,
 
         Returns
         -------
-        numpy.ndarray of shape (n_ts, n_segments, d)
-            PAA-Transformed dataset
+        numpy.ndarray of shape (n_ts, output_size, 1)
+            Matrix-Profile-Transformed dataset. `ouput_size` is equal to 
+            `sz - subsequence_length + 1`
         """
-        X = check_array(X, allow_nd=True)
-        X = check_dims(X)
-        return self.fit(X)._transform(X)
+        return self.fit(X).transform(X)
+
+    def _more_tags(self):
+        return {'allow_nan': True, 'allow_variable_length': True}
