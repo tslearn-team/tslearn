f7ba3b26fec453ce7e0f1faa352e5058af1053b8
Gilles Vandewiele <givdwiel.vandewiele@ugent.be>
2020-06-11 10:48:38 +0200

    plot tweaks

--- tslearn/docs/examples/misc/plot_distance_and_matrix_profile.py
+++ tslearn/docs/examples/misc/plot_distance_and_matrix_profile.py
@@ -14,6 +14,7 @@ extracted from which is equal to zero.
 
 import numpy
 import matplotlib.patches as patches
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 import matplotlib.pyplot as plt
 
 from tslearn.datasets import CachedDatasets
@@ -30,7 +31,8 @@ numpy.random.seed(42)
 X_train, y_train, _, _ = CachedDatasets().load_dataset("Trace")
 
 # Normalize the time series
-X_train = TimeSeriesScalerMeanVariance().fit_transform(X_train)
+scaler = TimeSeriesScalerMeanVariance()
+X_train = scaler.fit_transform(X_train)
 
 # Take the first time series
 ts = X_train[0, :, :]
@@ -45,56 +47,76 @@ matrix_profiler = MatrixProfile(subsequence_length=subseq_len, scale=True)
 mp = matrix_profiler.fit_transform([ts]).flatten()
 
 # Create a grid for our plots
-fig = plt.figure(constrained_layout=True)
-gs = fig.add_gridspec(7, 4)
-fig_ax1 = fig.add_subplot(gs[:3, :])
-fig_ax2 = fig.add_subplot(gs[3, 0])
-fig_ax3 = fig.add_subplot(gs[3, 1:])
-fig_ax4 = fig.add_subplot(gs[4:, :])
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
 
 # Plot our timeseries
-fig_ax1.plot(ts, c='b', label='time series')
-fig_ax1.add_patch(patches.Rectangle((start, numpy.min(ts) - 0.1), subseq_len,
-                                    numpy.max(ts) - numpy.min(ts) + 0.2,
-                                    facecolor='b', alpha=0.25,
-                                    label='segment'))
-fig_ax1.axvline(start, c='b', linestyle='--', lw=2, alpha=0.5,
-	            label='segment start')
-fig_ax1.legend(loc='lower right', ncol=4, fontsize=8)
-fig_ax1.set_title('The time series')
+ax1.plot(ts, c='b', label='time series')
+ax1.add_patch(patches.Rectangle((start, numpy.min(ts) - 0.1), subseq_len,
+                                numpy.max(ts) - numpy.min(ts) + 0.2,
+                                facecolor='b', alpha=0.25,
+                                label='segment'))
+ax1.axvline(start, c='b', linestyle='--', lw=2, alpha=0.5,
+            label='segment start')
+ax1.legend(loc='lower right', ncol=4, fontsize=8,
+           handletextpad=0.1, columnspacing=0.5)
+ax1.set_title('The time series')
 
-# Plot our segment
-fig_ax2.plot(segment, c='b')
-fig_ax2.set_title('Segment')
+# Inset plot with our segment
+fig_ax_in = ax1.inset_axes([0.5, 0.55, 0.2, 0.4])
+fig_ax_in.plot(scaler.fit_transform(segment.reshape(1, -1, 1))[0], c='b')
+ax1.indicate_inset(inset_ax=fig_ax_in, transform=ax1.transData,
+                   bounds=[start, numpy.min(ts) - 0.1, subseq_len,
+                           numpy.max(ts) - numpy.min(ts) + 0.2],
+                   linestyle='--', alpha=0.75)
+fig_ax_in.tick_params(labelleft=False, labelbottom=False)
+fig_ax_in.xaxis.set_visible(False)
+fig_ax_in.yaxis.set_visible(False)
 
 # Calculate a distance profile, which represents the distance from each
 # subsequence of the time series and the segment
 distances = []
-scaler = TimeSeriesScalerMeanVariance()
 for i in range(len(ts) - subseq_len):
     scaled_ts = scaler.fit_transform(ts[i:i+subseq_len].reshape(1, -1, 1))
     scaled_segment = scaler.fit_transform(segment.reshape(1, -1, 1))
     distances.append(numpy.linalg.norm(scaled_ts - scaled_segment))
 
-second_min_ix = numpy.argsort(distances)[1]
-second_min = distances[second_min_ix]
+# Mask out the distances in the trivial match zone, get the nearest
+# neighbor and put the old distances back in place so we can plot them.
+distances = numpy.array(distances)
+mask = list(range(start - subseq_len // 4, start + subseq_len // 4))
+old_distances = distances[mask]
+distances[mask] = numpy.inf
+nearest_neighbor = numpy.argmin(distances)
+dist_nn = distances[nearest_neighbor]
+distances[mask] = old_distances
 
 # Plot our distance profile
-fig_ax3.plot(distances, c='b')
-fig_ax3.set_title('Distance profile')
-fig_ax3.scatter(second_min_ix, distances[second_min_ix],
-                c='r', marker='x', s=50,
-                label='2nd min dist = {}'.format(numpy.around(second_min, 3)))
-fig_ax3.legend(loc='lower right', fontsize=8)
+ax2.plot(distances, c='b')
+ax2.set_title('Segment distance profile')
+dist_diff = numpy.max(distances) - numpy.min(distances)
+ax2.add_patch(patches.Rectangle((start - subseq_len // 4,
+                                 numpy.min(distances) - 0.1),
+                                subseq_len // 2,
+                                dist_diff + 0.2,
+                                facecolor='r', alpha=0.5,
+                                label='exclusion zone'))
+ax2.scatter(nearest_neighbor, dist_nn, c='r', marker='x', s=50,
+            label='neighbor dist = {}'.format(numpy.around(dist_nn, 3)))
+ax2.axvline(start, c='b', linestyle='--', lw=2, alpha=0.5,
+            label='segment start')
+ax2.legend(loc='lower right', fontsize=8, ncol=3,
+           handletextpad=0.1, columnspacing=0.5)
 
 # Plot our matrix profile
-fig_ax4.plot(mp, c='b')
-fig_ax4.set_title('Matrix profile')
-fig_ax4.scatter(start, mp[second_min_ix],
-                c='r', marker='x', s=75,
-                label='MP segment = {}'.format(numpy.around(mp[start], 3)))
-fig_ax4.axvline(start, c='b', linestyle='--', lw=2, alpha=0.5,
-	            label='segment start')
-fig_ax4.legend(loc='lower right', fontsize=8)
+ax3.plot(mp, c='b')
+ax3.set_title('Matrix profile')
+ax3.scatter(start, mp[start],
+            c='r', marker='x', s=75,
+            label='MP segment = {}'.format(numpy.around(mp[start], 3)))
+ax3.axvline(start, c='b', linestyle='--', lw=2, alpha=0.5,
+            label='segment start')
+ax3.legend(loc='lower right', fontsize=8,
+           handletextpad=0.1, columnspacing=0.25)
 
+plt.tight_layout()
 plt.show()
