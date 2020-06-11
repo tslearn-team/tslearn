5a8bb8aa75c04478441eb11a7ed253342ddf2be1
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2020-06-11 12:20:39 +0200

    Fine tune look

--- tslearn/docs/examples/classification/plot_early_classification.py
+++ tslearn/docs/examples/classification/plot_early_classification.py
@@ -6,16 +6,17 @@ Early Classification
 This example presents the concept of early classification.
 
 Early classifiers are implemented in the 
-:ref:`tslearn.early_classification <mod-metrics>` module and in this example 
+:mod:`tslearn.early_classification` module and in this example 
 we use the method from [1].
 
 
 [1] A. Dachraoui, A. Bondu & A. Cornuejols. Early classification of time
-    series as a non myopic sequential decision making problem. ECML/PKDD 2015
+series as a non myopic sequential decision making problem. ECML/PKDD 2015
 """
 
 # Author: Romain Tavenard
 # License: BSD 3 clause
+# sphinx_gallery_thumbnail_number = 2
 
 import numpy
 import matplotlib.pyplot as plt
@@ -31,7 +32,7 @@ def plot_partial(time_series, t, y_true=0, y_pred=0):
              time_series[t+1:].ravel(),
              linestyle="dashed", color=color)
     plt.axvline(x=t, color=color)
-    plt.text(x=t - 23, y=time_series.max() - .25, s="Prediction time")
+    plt.text(x=t - 20, y=time_series.max() - .25, s="Prediction time")
     plt.title(
         "Sample of class {} predicted as class {}".format(y_true, y_pred)
     )
@@ -70,7 +71,7 @@ plot_partial(X_test[ts_idx], t, y_test[ts_idx], preds[ts_idx])
 
 
 plt.subplot(2, 1, 2)
-ts_idx = 4
+ts_idx = 9
 t = times[ts_idx]
 plot_partial(X_test[ts_idx], t, y_test[ts_idx], preds[ts_idx])
 plt.tight_layout()
