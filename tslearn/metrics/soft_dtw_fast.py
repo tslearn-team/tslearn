66e8adaee99b9042a72eef24e678b6a84c38818c
Yann Cabanes <yann.cabanes@gmail.com>
2022-09-02 22:34:23 -0400

    Add parallel option in jit decorator of function _jacobian_product_sq_euc

--- tslearn/metrics/soft_dtw_fast.py
+++ tslearn/metrics/soft_dtw_fast.py
@@ -9,7 +9,9 @@ from numba import jit, njit, prange, float64
 DBL_MAX = np.finfo("double").max
 
 
-"""njit --> Ok
+"""parallel --> Ok, useless
+
+The keyword argument 'parallel=True' was specified but no transformation for parallel execution was possible.
 
 tslearn/metrics/soft_dtw_fast.py:73:            R[i, j] = D[i - 1, j - 1] + _softmin3(
 """
@@ -45,7 +47,20 @@ def _softmin3(a, b, c, gamma):
     return softmin_value
 
 
-"""njit --> Ok
+"""parallel --> Fail
+
+FAILED tslearn/tests/test_barycenters.py::test_softdtw_barycenter - AssertionError: 
+FAILED tslearn/tests/test_clustering.py::test_kmeans - AssertionError: 
+FAILED tslearn/tests/test_metrics.py::test_gak - AssertionError: 
+FAILED tslearn/tests/test_metrics.py::test_symmetric_cdist - AssertionError: 
+FAILED tslearn/tests/test_neighbors.py::test_constrained_paths - AssertionError:
+
+E       Mismatched elements: 6 / 15 (40%)
+E       Max absolute difference: 2
+E       Max relative difference: 1.
+E        x: array([2, 2, 0, 0, 2, 2, 0, 0, 2, 2, 2, 2, 2, 0, 2])
+E        y: array([2, 2, 0, 0, 2, 0, 2, 0, 2, 0, 1, 0, 2, 2, 2])
+
 
 tslearn/metrics/softdtw_variants.py:653:        _soft_dtw(self.D, self.R_, gamma=self.gamma)
 """
@@ -80,7 +95,19 @@ def _soft_dtw(D, R, gamma):
             )
 
 
-"""njit --> Ok
+"""parallel --> Fail
+
+FAILED tslearn/tests/test_barycenters.py::test_softdtw_barycenter - AssertionError: 
+
+E       Mismatched elements: 30 / 30 (100%)
+E       Max absolute difference: 0.49917453
+E       Max relative difference: 5.38180559
+E        x: array([[ 0.207017, -0.075996,  0.00961 ],
+E              [-0.372183, -0.164373, -0.385064],
+E              [ 0.018195,  0.167337, -0.164295],...
+E        y: array([[ 0.280494, -0.011908, -0.062284],
+E              [-0.670971, -0.107371, -0.338678],
+E              [ 0.293808,  0.047417,  0.327185],...
 
 tslearn/metrics/softdtw_variants.py:682:        _soft_dtw_grad(D, self.R_, E, gamma=self.gamma)
 """
@@ -112,7 +139,9 @@ def _soft_dtw_grad(D, R, E, gamma):
     D[m, n] = 0
 
     # DP recursion.
-    for j in prange(n, 0, -1):  # ranges from n to 1
+    # for j in prange(n, 0, -1):  # ranges from n to 1
+    for k in prange(n):
+        j = n - k
         for i in range(m, 0, -1):  # ranges from m to 1
             a = np.exp((R[i + 1, j] - R[i, j] - D[i, j - 1]) / gamma)
             b = np.exp((R[i, j + 1] - R[i, j] - D[i - 1, j]) / gamma)
@@ -120,14 +149,14 @@ def _soft_dtw_grad(D, R, E, gamma):
             E[i, j] = E[i + 1, j] * a + E[i, j + 1] * b + E[i + 1, j + 1] * c
 
 
-"""njit --> Ok
+"""parallel --> Ok
 
 tslearn/metrics/softdtw_variants.py:735:        _jacobian_product_sq_euc(self.X, self.Y, E.astype(numpy.float64), G)
 """
 
 
 # @njit(parallel=True)
-@njit((float64[:, :], float64[:, :], float64[:, :], float64[:, :]))
+@njit((float64[:, :], float64[:, :], float64[:, :], float64[:, :]), parallel=True)
 def _jacobian_product_sq_euc(X, Y, E, G):
     """Compute the square Euclidean product between the Jacobian
     (a linear map from m x d to m x n) and a matrix E.
