35e3e5b77fbb1aace5062c5324db72df978d1e49
Yann Cabanes <yann.cabanes@gmail.com>
2022-11-10 11:56:06 -0500

    Rename GenericBackend into Backend

--- tslearn/try_backends_tslearn_functions.py
+++ tslearn/try_backends_tslearn_functions.py
@@ -1,6 +1,6 @@
 """Simple functions representing the tslearn functions."""
 
-from try_backends_definition import GenericBackend
+from try_backends_definition import Backend
 
 
 def add(x, y):
@@ -8,23 +8,23 @@ def add(x, y):
 
 
 def exp(x):
-    be = GenericBackend(x)
+    be = Backend(x)
     return be.exp(x)
 
 
 def log(x):
-    be = GenericBackend(x)
+    be = Backend(x)
     return be.log(x)
 
 
 def _inv_matrix_aux(x, be=None):
     if be is None:
-        be = GenericBackend(x)
+        be = Backend(x)
     return be.linalg.inv(x)
 
 
 def inv_matrices_main(x):
-    be = GenericBackend(x)
+    be = Backend(x)
     inv_matrices = be.zeros(be.shape(x), x.dtype)
     for i_matrix in range(x.shape[0]):
         inv_matrices[i_matrix] = _inv_matrix_aux(x[i_matrix], be=be)
