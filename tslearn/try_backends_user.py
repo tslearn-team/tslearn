c415fae7e4c65b84af61956925e0278e92210204
Yann Cabanes <yann.cabanes@gmail.com>
2022-11-09 21:25:32 -0500

    Improve GenericBackend draft

--- tslearn/try_backends_user.py
+++ tslearn/try_backends_user.py
@@ -1,12 +1,18 @@
+"""Functions used by the user calling the tslearn functions."""
+
 import numpy as np
 import torch
-from try_backends_tslearn_functions import add, exp, log
+from try_backends_definition import GenericBackend
+from try_backends_tslearn_functions import add, exp, inv_matrices_main, log
 
 x_numpy = np.array([1.0, 2.0, 0.1])
 y_numpy = np.array([2.0, 3.0, -4.0])
 
 x_torch = torch.tensor([1.0, 2.0, 3.0])
 y_torch = np.array([1.0, 2.0, -10.0])
+
+matrices_numpy = np.arange(8, dtype=float).reshape((2, 2, 2))
+matrices_torch = torch.arange(8, dtype=float).reshape((2, 2, 2))
 
 print("add")
 print(add(x_numpy, y_numpy))
@@ -19,3 +25,15 @@ print(exp(x_torch))
 print("log")
 print(log(x_numpy))
 print(log(x_torch))
+
+print("inv matrices")
+print(inv_matrices_main(matrices_numpy))
+print(inv_matrices_main(matrices_torch))
+
+be = GenericBackend()
+b = be.array([[1, 2], [3, 4]])
+print(b)
+
+be = GenericBackend("pytorch")
+a = be.array([[1, 2], [3, 4]])
+print(a)
