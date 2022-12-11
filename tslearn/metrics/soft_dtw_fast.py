c22a7768a02eb96cfe845ef04626903a52313a1d
Yann Cabanes <yann.cabanes@gmail.com>
2022-12-11 11:09:41 -0500

    Remove useless tests codes from soft_dtw_fast.py

--- tslearn/metrics/soft_dtw_fast.py
+++ tslearn/metrics/soft_dtw_fast.py
@@ -3,8 +3,6 @@
 
 # encoding: utf-8
 
-from numba import njit, prange
-
 from tslearn.backend.backend import Backend
 
 
@@ -38,9 +36,6 @@ def _softmin3(a, b, c, gamma, be=None):
     return softmin_value
 
 
-njit_softmin3 = njit(fastmath=True)(_softmin3)
-
-
 def _soft_dtw(D, R, gamma, be=None):
     """Compute soft dynamic time warping.
 
@@ -52,11 +47,6 @@ def _soft_dtw(D, R, gamma, be=None):
     """
     if be is None:
         be = Backend(D)
-
-    if be.is_numpy:
-        softmin3 = njit_softmin3
-    else:
-        softmin3 = _softmin3
 
     m = D.shape[0]
     n = D.shape[1]
@@ -70,12 +60,9 @@ def _soft_dtw(D, R, gamma, be=None):
     for i in range(1, m + 1):
         for j in range(1, n + 1):
             # D is indexed starting from 0.
-            R[i, j] = D[i - 1, j - 1] + softmin3(
+            R[i, j] = D[i - 1, j - 1] + _softmin3(
                 R[i - 1, j], R[i - 1, j - 1], R[i, j - 1], gamma, be=be
             )
-
-
-njit_soft_dtw = njit(parallel=True, fastmath=True)(_soft_dtw)
 
 
 def _soft_dtw_grad(D, R, E, gamma, be=None):
@@ -112,9 +99,6 @@ def _soft_dtw_grad(D, R, E, gamma, be=None):
             E[i, j] = E[i + 1, j] * a + E[i, j + 1] * b + E[i + 1, j + 1] * c
 
 
-jit_soft_dtw_grad = njit(parallel=True, fastmath=True)(_soft_dtw_grad)
-
-
 def _jacobian_product_sq_euc(X, Y, E, G):
     """Compute the square Euclidean product between the Jacobian
     (a linear map from m x d to m x n) and a matrix E.
@@ -134,76 +118,7 @@ def _jacobian_product_sq_euc(X, Y, E, G):
     n = Y.shape[0]
     d = X.shape[1]
 
-    for i in prange(m):
+    for i in range(m):
         for j in range(n):
             for k in range(d):
                 G[i, k] += E[i, j] * 2 * (X[i, k] - Y[j, k])
-
-
-njit_jacobian_product_sq_euc = njit(parallel=True, fastmath=True)(
-    _jacobian_product_sq_euc
-)
-
-
-import numpy as np
-from numba import njit
-from tslearn.backend import Backend
-
-x = np.array([1, 2, 3])
-y = np.array([1, 2.2, -3])
-
-
-@njit()
-def addition(x, y):
-    return np.add(x, y)
-
-
-print(addition(x, y))
-
-
-@njit()
-def main_addition(backend, x, y):
-    # if backend == np:
-    #     print('backend == np')
-    return np.add(x, y)
-    return addition(x, y)
-
-
-backend = Backend("numpy")
-
-print(main_addition(backend, x, y))
-print(main_addition(x, y))
-
-
-# class TestNjit():
-#     def __init__(self):
-#         self.add = addition
-#
-#     # @staticmethod
-#     # @njit()
-#     # def add(x, y):
-#     #     return np.add(x, y)
-#
-#
-# backend = Backend("numpy")
-# print(backend)
-#
-# @njit()
-# def main_function(x, y, be=None):
-#     print('be in main')
-#     print(be)
-#     print(be is None)
-#     if be is None:
-#         print('be is None')
-#         be = Backend(x)
-#     # return addition(x, y)
-#     # test_njit = TestNjit()
-#     # return test_njit.add(x, y)
-#     return be.exp(x + y)
-#
-#
-# a = TestNjit().add(x, y)
-# print(a)
-#
-# b = main_function(x, y, backend)
-# print(b)
