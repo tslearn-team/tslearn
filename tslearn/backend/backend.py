93c6adcea15eee1b20740754014314dda743e396
Yann Cabanes <yann.cabanes@gmail.com>
2022-12-12 02:04:39 -0500

    Improve the backends

--- tslearn/backend/backend.py
+++ tslearn/backend/backend.py
@@ -31,7 +31,7 @@ def select_backend(data):
 
 
 def backend_to_string(backend):
-    if "NumPY" in f"{backend}":
+    if "NumPy" in f"{backend}":
         return "numpy"
     return "pytorch"
 
@@ -55,29 +55,51 @@ class Backend(object):
         self.backend_string = backend_to_string(self.backend)
         self.is_numpy = self.backend_string == "numpy"
         self.is_pytorch = self.backend_string == "pytorch"
+
         self.linalg = self.backend.linalg
+        self.random = self.backend.random
+
+        self.int8 = self.backend.int8
+        self.int16 = self.backend.int16
+        self.int32 = self.backend.int32
+        self.int64 = self.backend.int64
+        self.float32 = self.backend.float32
+        self.float64 = self.backend.float64
+        self.complex64 = self.backend.complex64
+        self.complex128 = self.backend.complex128
+
+        self.all = self.backend.all
+        self.array = self.backend.array
         self.dbl_max = self.backend.dbl_max
-
-    def array(self, data, dtype=None):
-        return self.backend.array(data, dtype=dtype)
-
-    def exp(self, data, dtype=None):
-        return self.backend.exp(data, dtype=dtype)
+        self.diag = self.backend.diag
+        self.empty = self.backend.empty
+        self.exp = self.backend.exp
+        self.hstack = self.backend.hstack
+        self.inf = self.backend.inf
+        self.is_array = self.backend.is_array
+        self.iscomplex = self.backend.iscomplex
+        self.is_float = self.backend.is_float
+        self.is_float32 = self.backend.is_float32
+        self.is_float64 = self.backend.is_float64
+        self.isnan = self.backend.isnan
+        self.log = self.backend.log
+        self.max = self.backend.max
+        self.mean = self.backend.mean
+        self.median = self.backend.median
+        self.min = self.backend.min
+        self.nan = self.backend.nan
+        self.ndim = self.backend.ndim
+        self.shape = self.backend.shape
+        self.sqrt = self.backend.sqrt
+        self.to_float = self.backend.to_float
+        self.to_float32 = self.backend.to_float32
+        self.to_float64 = self.backend.to_float64
+        self.to_numpy = self.backend.to_numpy
+        self.vstack = self.backend.vstack
+        self.zeros = self.backend.zeros
 
     def get_backend(self):
         return self.backend
 
-    def log(self, data, dtype=None):
-        return self.backend.log(data, dtype=dtype)
-
     def set_backend(self, data=None):
         self.backend = select_backend(data)
-
-    def shape(self, data):
-        return self.backend.shape(data)
-
-    def to_numpy(self, data):
-        return self.backend.to_numpy(data)
-
-    def zeros(self, shape, dtype=None):
-        return self.backend.zeros(shape, dtype=dtype)
--- tslearn/backend/numpy_backend.py
+++ tslearn/backend/numpy_backend.py
@@ -10,34 +10,82 @@ class NumPyBackend(BaseBackend):
 
     def __init__(self):
         self.linalg = NumPyLinalg()
+        self.random = NumPyRandom()
+
+        self.int8 = _np.int8
+        self.int16 = _np.int16
+        self.int32 = _np.int32
+        self.int64 = _np.int64
+        self.float32 = _np.float32
+        self.float64 = _np.float64
+        self.complex64 = _np.complex64
+        self.complex128 = _np.complex128
+
+        self.all = _np.all
+        self.array = _np.array
         self.dbl_max = _np.finfo("double").max
+        self.diag = _np.diag
+        self.empty = _np.empty
+        self.exp = _np.exp
+        self.hstack = _np.hstack
+        self.inf = _np.inf
+        self.iscomplex = _np.iscomplex
+        self.isnan = _np.isnan
+        self.log = _np.log
+        self.max = _np.max
+        self.mean = _np.mean
+        self.median = _np.median
+        self.min = _np.min
+        self.nan = _np.nan
+        self.shape = _np.shape
+        self.sqrt = _np.sqrt
+        self.vstack = _np.vstack
+        self.zeros = _np.zeros
 
     @staticmethod
-    def array(data, dtype=None):
-        return _np.array(data, dtype=dtype)
+    def is_array(x):
+        return type(x) is _np.ndarray
 
     @staticmethod
-    def exp(data, dtype=None):
-        return _np.exp(data)
+    def is_float(x):
+        return isinstance(x, (_np.floating, float))
 
     @staticmethod
-    def log(data, dtype=None):
-        return _np.log(data)
+    def is_float32(x):
+        return isinstance(x, _np.float32)
 
     @staticmethod
-    def shape(data):
-        return _np.shape(data)
+    def is_float64(x):
+        return isinstance(x, _np.float64)
+
+    @staticmethod
+    def ndim(x):
+        return x.ndim
+
+    @staticmethod
+    def to_float(x):
+        return x.astype(float)
+
+    @staticmethod
+    def to_float32(x):
+        return x.astype(_np.float32)
+
+    @staticmethod
+    def to_float64(x):
+        return x.astype(_np.float64)
 
     @staticmethod
     def to_numpy(x):
         return x
 
-    @staticmethod
-    def zeros(shape, dtype=None):
-        return _np.zeros(shape, dtype=dtype)
-
 
 class NumPyLinalg:
-    @staticmethod
-    def inv(x):
-        return _np.linalg.inv(x)
+    def __init__(self):
+        self.inv = _np.linalg.inv
+
+
+class NumPyRandom:
+    def __init__(self):
+        self.rand = _np.random.rand
+        self.randint = _np.random.randint
+        self.randn = _np.random.randn
