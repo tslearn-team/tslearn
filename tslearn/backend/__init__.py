fbd996f0329836d103921b1195c7e8ae58ab61c6
Yann Cabanes <yann.cabanes@gmail.com>
2023-09-14 17:51:57 -0400

    Add cast function in backend

--- tslearn/backend/__init__.py
+++ tslearn/backend/__init__.py
@@ -3,10 +3,11 @@ The :mod:`tslearn.backend` module provides multiple backends.
 The backends provided are NumPy and PyTorch.
 """
 
-from .backend import Backend, instantiate_backend, select_backend
+from .backend import Backend, cast, instantiate_backend, select_backend
 
 __all__ = [
     "Backend",
+    "cast",
     "instantiate_backend",
     "select_backend",
 ]
