3ead4614e4162612604814bd3e3f1b8d4de1962c
Yann Cabanes <yann.cabanes@gmail.com>
2023-11-30 12:11:14 -0500

    Change alias name for autograd.numpy

--- tslearn/backend/autograd_backend.py
+++ tslearn/backend/autograd_backend.py
@@ -19,7 +19,7 @@ from scipy.spatial.distance import cdist, pdist
 from sklearn.metrics.pairwise import euclidean_distances, pairwise_distances
 
 try:
-    import autograd.numpy as _adnp
+    import autograd.numpy as _agnp
 
     HAS_AUTOGRAD = True
 except ImportError:
@@ -46,57 +46,57 @@ else:
             self.random = AutogradRandom()
             self.testing = AutogradTesting()
 
-            self.int8 = _adnp.int8
-            self.int16 = _adnp.int16
-            self.int32 = _adnp.int32
-            self.int64 = _adnp.int64
-            self.float32 = _adnp.float32
-            self.float64 = _adnp.float64
-            self.complex64 = _adnp.complex64
-            self.complex128 = _adnp.complex128
+            self.int8 = _agnp.int8
+            self.int16 = _agnp.int16
+            self.int32 = _agnp.int32
+            self.int64 = _agnp.int64
+            self.float32 = _agnp.float32
+            self.float64 = _agnp.float64
+            self.complex64 = _agnp.complex64
+            self.complex128 = _agnp.complex128
 
-            self.abs = _adnp.abs
-            self.all = _adnp.all
-            self.any = _adnp.any
-            self.arange = _adnp.arange
-            self.argmax = _adnp.argmax
-            self.argmin = _adnp.argmin
-            self.array = _adnp.array
+            self.abs = _agnp.abs
+            self.all = _agnp.all
+            self.any = _agnp.any
+            self.arange = _agnp.arange
+            self.argmax = _agnp.argmax
+            self.argmin = _agnp.argmin
+            self.array = _agnp.array
             self.cdist = cdist
-            self.ceil = _adnp.ceil
-            self.dbl_max = _adnp.finfo("double").max
-            self.diag = _adnp.diag
-            self.empty = _adnp.empty
-            self.exp = _adnp.exp
-            self.eye = _adnp.eye
-            self.floor = _adnp.floor
-            self.full = _adnp.full
-            self.hstack = _adnp.hstack
-            self.inf = _adnp.inf
-            self.iscomplex = _adnp.iscomplex
-            self.isfinite = _adnp.isfinite
-            self.isnan = _adnp.isnan
-            self.log = _adnp.log
-            self.max = _adnp.max
-            self.mean = _adnp.mean
-            self.median = _adnp.median
-            self.min = _adnp.min
-            self.nan = _adnp.nan
+            self.ceil = _agnp.ceil
+            self.dbl_max = _agnp.finfo("double").max
+            self.diag = _agnp.diag
+            self.empty = _agnp.empty
+            self.exp = _agnp.exp
+            self.eye = _agnp.eye
+            self.floor = _agnp.floor
+            self.full = _agnp.full
+            self.hstack = _agnp.hstack
+            self.inf = _agnp.inf
+            self.iscomplex = _agnp.iscomplex
+            self.isfinite = _agnp.isfinite
+            self.isnan = _agnp.isnan
+            self.log = _agnp.log
+            self.max = _agnp.max
+            self.mean = _agnp.mean
+            self.median = _agnp.median
+            self.min = _agnp.min
+            self.nan = _agnp.nan
             self.pairwise_distances = pairwise_distances
             self.pairwise_euclidean_distances = euclidean_distances
             self.pdist = pdist
-            self.reshape = _adnp.reshape
-            self.round = _adnp.round
-            self.shape = _adnp.shape
-            self.sqrt = _adnp.sqrt
-            self.sum = _adnp.sum
-            self.tril = _adnp.tril
-            self.tril_indices = _adnp.tril_indices
-            self.triu = _adnp.triu
-            self.triu_indices = _adnp.triu_indices
-            self.vstack = _adnp.vstack
-            self.zeros = _adnp.zeros
-            self.zeros_like = _adnp.zeros_like
+            self.reshape = _agnp.reshape
+            self.round = _agnp.round
+            self.shape = _agnp.shape
+            self.sqrt = _agnp.sqrt
+            self.sum = _agnp.sum
+            self.tril = _agnp.tril
+            self.tril_indices = _agnp.tril_indices
+            self.triu = _agnp.triu
+            self.triu_indices = _agnp.triu_indices
+            self.vstack = _agnp.vstack
+            self.zeros = _agnp.zeros
+            self.zeros_like = _agnp.zeros_like
 
         @staticmethod
         def belongs_to_backend(x):
@@ -116,19 +116,19 @@ else:
 
         @staticmethod
         def is_array(x):
-            return type(x) is _adnp.ndarray
+            return type(x) is _agnp.ndarray
 
         @staticmethod
         def is_float(x):
-            return isinstance(x, (_adnp.floating, float))
+            return isinstance(x, (_agnp.floating, float))
 
         @staticmethod
         def is_float32(x):
-            return isinstance(x, _adnp.float32)
+            return isinstance(x, _agnp.float32)
 
         @staticmethod
         def is_float64(x):
-            return isinstance(x, _adnp.float64)
+            return isinstance(x, _agnp.float64)
 
         @staticmethod
         def ndim(x):
@@ -141,15 +141,15 @@ else:
 
     class AutogradLinalg:
         def __init__(self):
-            self.inv = _adnp.linalg.inv
-            self.norm = _adnp.linalg.norm
+            self.inv = _agnp.linalg.inv
+            self.norm = _agnp.linalg.norm
 
 
     class AutogradRandom:
         def __init__(self):
-            self.rand = _adnp.random.rand
-            self.randint = _adnp.random.randint
-            self.randn = _adnp.random.randn
+            self.rand = _agnp.random.rand
+            self.randint = _agnp.random.randint
+            self.randn = _agnp.random.randn
 
 
     class AutogradTesting:
