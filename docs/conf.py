713627de937bb9baabbe3e00232f49324614b62f
Yann Cabanes <yann.cabanes@gmail.com>
2024-06-28 16:15:44 -0400

    Remove empty line from conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,6 @@ rtd_version = os.environ.get('READTHEDOCS_VERSION', 'local')
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
