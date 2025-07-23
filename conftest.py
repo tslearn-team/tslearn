294e6820b263b72b7a67fde9070bde73b0e39571
charavelg <guillaume.charavel@inria.fr>
2025-07-23 13:39:05 +0200

    Do not rely on remote dataset fetching for CI to complete

--- conftest.py
+++ conftest.py
@@ -1,4 +1,8 @@
+import warnings
+
 import pytest
+
+from tslearn.datasets import UCR_UEA_datasets
 
 
 def pytest_collection_modifyitems(config, items):
@@ -32,5 +36,26 @@ def pytest_collection_modifyitems(config, items):
             if item.name in [
                 "tslearn.utils.to_cesium_dataset",
                 "tslearn.utils.from_cesium_dataset",
+            ]:
+                item.add_marker(skip_marker)
+
+    # Skip related doctests if UCR UEA datasets cannot be fetched
+    ucr_uea_datasets = True
+    try:
+        datasets = UCR_UEA_datasets()
+        datasets.cache_all()
+        ucr_uea_datasets = bool(datasets.list_datasets())
+    except Exception as exc:
+        warnings.warn("Error caching UCR UEA datasets: {}".format(exc))
+
+    if not ucr_uea_datasets:
+        warnings.warn("Skipping doctests requiring UCR UEA dataset download")
+        skip_marker = pytest.mark.skip(reason="Datasets not cached!")
+        for item in items:
+            if item.name in [
+                "tslearn.datasets.ucr_uea.UCR_UEA_datasets.list_datasets",
+                "tslearn.datasets.ucr_uea.UCR_UEA_datasets.list_multivariate_datasets",
+                "tslearn.datasets.ucr_uea.UCR_UEA_datasets.list_univariate_datasets",
+                "tslearn.datasets.ucr_uea.UCR_UEA_datasets.load_dataset",
             ]:
                 item.add_marker(skip_marker)
