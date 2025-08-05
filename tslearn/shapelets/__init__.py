94f37bca975a10fb5de622a9a54ba57b1e04331d
charavelg <guillaume.charavel@inria.fr>
2025-08-05 14:33:08 +0200

    Code smell

--- tslearn/shapelets/__init__.py
+++ tslearn/shapelets/__init__.py
@@ -3,7 +3,7 @@ The :mod:`tslearn.shapelets` module gathers Shapelet-based algorithms.
 
 It depends on the `keras` library (Keras3+ is required).
 
-**User guide:** See the :ref:`Shapelets <shapelets>` section for further 
+**User guide:** See the :ref:`Shapelets <shapelets>` section for further
 details.
 """
 
