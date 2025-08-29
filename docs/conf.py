bb4ce32cfa8bac4785cf65a903334946ff943772
charavelg <guillaume.charavel@inria.fr>
2025-08-29 08:56:21 +0200

    Dark theme logo (#585)
    Add new logos

--- docs/conf.py
+++ docs/conf.py
@@ -226,7 +226,7 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_static/tslearn_logo.svg"
+html_logo = "_static/tslearn_logo_white_background.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
